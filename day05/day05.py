1ee029b69be41f833456e47f4a23de55ba831f1e
Isaiah A <isaiah.althouse@gmail.com>
2022-12-05 12:17:13 -0700

    Day 5: class-based rewrite

--- day05/day05.py
+++ day05/day05.py
@@ -2,46 +2,33 @@ import sys
 import re
 
 
+class CargoManager:
+    def __init__(self, stacks):
+        self.stacks = stacks
+
+    def move_crates_individual(self, instructions):
+        for [count, start, end] in instructions:
+            for _ in range(count):
+                item = self.stacks[start].pop()
+                self.stacks[end].append(item)
+
+    def move_crates_multiple(self, instructions):
+        for [count, start, end] in instructions:
+            self.stacks[end] += self.stacks[start][-count:]
+            self.stacks[start] = self.stacks[start][:-count]
+
+
 def parse_drawing(drawing):
     drawing = drawing.split('\n')
     col_count = len(drawing[0])//4 + 1
-
-    state = [[] for _ in range(col_count)]
-
+    stacks = [[] for _ in range(col_count)]
     for line in drawing:
         for col, i in enumerate(range(col_count)):
             line_index = col + 1 + i * 3
             char = line[line_index]
             if char.isupper():
-                state[col].insert(0, char)
-
-    return state
-
-
-def parse_instruction(instruction):
-    res = [int(n) for n in re.findall('\d{1,}', instruction)]
-    if (len(res) == 3):
-        return res
-    else:
-        raise Exception('bad instruction input', instruction)
-
-
-def handle_instructions_v2(state, instructions):
-    for line in instructions:
-        [_count, _from, _to] = parse_instruction(line)
-        lifted = []
-        for _ in range(_count):
-            item = state[_from - 1].pop()
-            lifted.insert(0, item)
-        state[_to - 1] = state[_to - 1] + lifted
-
-
-def handle_instructions_v1(state, instructions):
-    for line in instructions:
-        [_count, _from, _to] = parse_instruction(line)
-        for _ in range(_count):
-            item = state[_from - 1].pop()
-            state[_to - 1].append(item)
+                stacks[col].insert(0, char)
+    return stacks
 
 
 def load_input(path):
@@ -50,20 +37,17 @@ def load_input(path):
         input = fp.read()
         [drawing, raw_instructions] = input.split('\n\n')
         for line in raw_instructions.split('\n'):
-            instructions.append(line)
-        return [drawing, instructions]
+            [cnt, start, end] = [int(n) for n in re.findall('\d{1,}', line)]
+            instructions.append([cnt, start - 1, end - 1])
+        return [parse_drawing(drawing), instructions]
 
 
 def main():
     path = sys.argv[1] if len(sys.argv) > 1 else 'input.txt'
-
-    [drawing, instructions] = load_input(path)
-
-    state = parse_drawing(drawing)
-
-    handle_instructions_v1(state, instructions)
-
-    print(''.join([n[-1] for n in state]))
+    [stacks, instructions] = load_input(path)
+    cargoManager = CargoManager(stacks)
+    cargoManager.move_crates_multiple(instructions)
+    print('result:', ''.join([n[-1] for n in cargoManager.stacks]))
 
 
 if __name__ == "__main__":
