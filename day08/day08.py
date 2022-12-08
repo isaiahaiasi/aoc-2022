0db1fbe546b3347a22fbf1f3b1c5ece6de09198d
Isaiah A <isaiah.althouse@gmail.com>
2022-12-08 10:43:38 -0700

    Day 8: further tidying

--- day08/day08.py
+++ day08/day08.py
@@ -1,53 +1,51 @@
 import sys
 
+DIRS = [[0, -1], [0, 1], [-1, 0], [1, 0]]
+
 
 class Solver:
     def __init__(self, input):
         self.forest = input
-        self.rotated_forest = list(zip(*input))
-        self.height = len(input)
-        self.width = len(input[0])
+        self.z_forest = list(zip(*input))
+        self.h = len(input)
+        self.w = len(input[0])
 
     def is_exterior(self, x, y):
-        return not (
-            x < self.height - 1
-            and y < self.width - 1
-            and x >= 1
-            and y >= 1
-        )
+        return x >= self.h - 1 or y >= self.w - 1 or x < 1 or y < 1
 
     def is_visible(self, x, y):
-        return min(
-            max(self.forest[y][0:x]),
-            max(self.forest[y][x + 1:self.width]),
-            max(self.rotated_forest[x][0:y]),
-            max(self.rotated_forest[x][y + 1:self.height])
-        ) < self.forest[y][x]
+        return (max(self.forest[y][0:x]) < self.forest[y][x]
+                or max(self.forest[y][x + 1:self.w]) < self.forest[y][x]
+                or max(self.z_forest[x][0:y]) < self.forest[y][x]
+                or max(self.z_forest[x][y + 1:self.h]) < self.forest[y][x])
 
     def visible_count(self):
         vis = 0
-        for i in range(self.height):
-            for j in range(self.width):
-                if self.is_exterior(i, j) or self.is_visible(i, j):
+        for y in range(self.h):
+            for x in range(self.w):
+                if self.is_exterior(x, y) or self.is_visible(x, y):
                     vis += 1
         return vis
 
+    def visibility_score(self, y, x):
+        score = 1
+        for v, h in DIRS:
+            step = 0
+            while True:
+                step += 1
+                step_y = y + (v * step)
+                step_x = x + (h * step)
+                if (self.is_exterior(step_x, step_y)
+                        or self.forest[step_y][step_x] >= self.forest[y][x]):
+                    break
+            score *= step
+        return score
+
     def best_local_visibility(self):
         best = 0
-        dirs = [[0, -1], [0, 1], [-1, 0], [1, 0]]
-        for i in range(1, self.height - 1):
-            for j in range(1, self.width - 1):
-                score = 1
-                for v, h in dirs:
-                    step = 0
-                    while True:
-                        step += 1
-                        y = i + (v * step)
-                        x = j + (h * step)
-                        if (self.is_exterior(x, y) or self.forest[y][x] >= self.forest[i][j]):
-                            break
-                    score *= step
-                best = max(best, score)
+        for i in range(1, self.h - 1):
+            for j in range(1, self.w - 1):
+                best = max(best, self.visibility_score(i, j))
         return best
 
 
@@ -62,8 +60,8 @@ def main():
 
     solver = Solver(input)
 
-    print('part A:', solver.visible_count())
-    print('part B:', solver.best_local_visibility())
+    print('part A:', solver.visible_count())  # 1845
+    print('part B:', solver.best_local_visibility())  # 230112
 
 
 if __name__ == "__main__":
