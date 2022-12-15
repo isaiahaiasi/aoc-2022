e7e5133bd662ac889c41f312dc133bb5894eb978
Isaiah A <isaiah.althouse@gmail.com>
2022-12-15 15:16:25 -0700

    Day 15: Reimpliment using interval merging

--- day15/day15.py
+++ day15/day15.py
@@ -1,77 +1,60 @@
 import sys
 import re
 
-
-def in_2d_range(pos, minpos, maxpos=None):
-    x, y = pos
-    minx, miny = minpos if maxpos else 0, 0
-    maxx, maxy = maxpos if maxpos else minpos
-    return x >= minx and y >= miny and x <= maxx and y <= maxy
+A_ROW = 2_000_000
+B_MAX = 4_000_000
 
 
-def get_manhatten(xa, ya, xb, yb):
-    return abs(xb - xa) + abs(yb - ya)
+def merge_intervals(intervals: list[tuple[int, int]]):
+    sorted_intervals = sorted(intervals, key=lambda x: x[0])
+    merged = [sorted_intervals[0]]
+    for b_start, b_end in sorted_intervals[1:]:
+        a_start, a_end = merged[-1]
+        if b_start <= a_end:
+            merged[-1] = a_start, max(a_end, b_end)
+        else:
+            merged.append((b_start, b_end))
+    return merged
 
 
 class Solver:
-    def __init__(self, sensors: dict, beacons: set):
+    def __init__(self,
+                 sensors: dict[tuple[int, int], tuple[int, int]],
+                 beacons: set[tuple[int, int]]):
         self.sensors = sensors
         self.beacons = beacons
 
-    def walk_edge(self, sensor: tuple[int, int]):
-        sx, sy = sensor
-        sensor_range = self.sensors[sensor] + 1  # one more to be outside edge
-        # NW
-        for i in range(sensor_range):
-            yield sx - i, sy - sensor_range + i
-        # SW
-        for i in range(sensor_range):
-            yield sx - sensor_range + i, sy + i
-        # SE
-        for i in range(sensor_range):
-            yield sx + i, sy + sensor_range - i
-        # NE
-        for i in range(sensor_range):
-            yield sx + sensor_range - i, sy - i
-
-    def has_sensor_coverage(self, x, y):
-        for spos, db in self.sensors.items():
-            sbx, sby = spos
-            sensor_delta = db - get_manhatten(sbx, sby, x, y)
-            if sensor_delta >= 0:
-                return True
-        return False
-
     def get_row_coverage(self, y):
-        coverage_set = set()
+        coverage_intervals = []
         for sensor, delta in self.sensors.items():
             sx, sy = sensor
             coverage = (delta - abs(sy - y)) * 2 + 1
-            if coverage <= 0:
-                continue
-            cx = [x for x in range(sx - coverage // 2 - 1, sx + coverage // 2)]
-            for x in cx:
-                coverage_set.add(x)
-        for bx, by in self.beacons:
-            if by == y:
-                coverage_set.discard(bx)
-        for bx, by in self.sensors.keys():
-            if by == y:
-                coverage_set.discard(bx)
-        return len(coverage_set)
+            if coverage > 0:
+                interval = (sx - coverage // 2, sx + coverage // 2 + 1)
+                coverage_intervals.append(interval)
+        return merge_intervals(coverage_intervals)
 
-    # walk edge of each diamond and find where it doesn't intersect another
-    def get_distress_position(self, cmax):
-        for sensor in self.sensors.keys():
-            for x, y in self.walk_edge(sensor):
-                if (in_2d_range((x, y), (cmax, cmax))
-                        and not self.has_sensor_coverage(x, y)):
-                    return (x, y)
+    def get_positions_without_beacon_count(self, y):
+        row_intervals = self.get_row_coverage(y)
+        beacons_in_row = [b[0] for b in self.beacons if b[1] == y]
+        count = 0
+        for start, end in row_intervals:
+            b_adj = 0
+            for bx in beacons_in_row:
+                if bx >= start and bx <= end:
+                    b_adj += 1
+            count += end - start - b_adj
+        return count
+
+    def get_distress_position(self):
+        for row in range(B_MAX):
+            intervals = self.get_row_coverage(row)
+            if len(intervals) > 1:
+                return intervals[0][1], row
 
     def get_distress_frequency(self):
-        magic_num = 4_000_000  # why is it range and multiplier? magic!
-        x, y = self.get_distress_position(magic_num)
-        return x * magic_num + y
+        x, y = self.get_distress_position()
+        return x * B_MAX + y
 
 
 def load_input(path):
@@ -82,7 +65,7 @@ def load_input(path):
             sx, sy, bx, by = map(int, re.findall('[\-]?\d+', line.strip()))
             beacon = (bx, by)
             beacons.add(beacon)
-            sensors[(sx, sy)] = get_manhatten(sx, sy, bx, by)
+            sensors[(sx, sy)] = abs(bx - sx) + abs(by - sy)
     return sensors, beacons
 
 
@@ -92,13 +75,8 @@ def main():
 
     solver = Solver(sensors, beacons)
 
-    result_a = solver.get_row_coverage(2_000_000)
-    print("PART A:", result_a)
-    print("part A match:", result_a == 6078701)
-
-    result_b = solver.get_distress_frequency()
-    print("PART B:", result_b)
-    print("part B match:", result_b == 12567351400528)
+    print("PART A", solver.get_positions_without_beacon_count(A_ROW))
+    print("PART B:", solver.get_distress_frequency())
 
 
 if __name__ == "__main__":
